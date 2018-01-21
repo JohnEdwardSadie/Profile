d36c8f3a67e35f049cf4eca7af4bbcce24fff889
princesadie <princesadie@gmail.com>
2018-01-21 17:52:45 -0600

    Install react-pdf, update Resume.jsx, and modify CSS rules

--- src/Components/Resume.jsx
+++ src/Components/Resume.jsx
@@ -1,8 +1,20 @@
 import React, { Component } from 'react';
+import { Document, Page } from 'react-pdf';
+import ResumePDF from './JohnSadie.pdf';
 import Title from './Title.jsx';
 
 class Resume extends Component {
+  state = {
+    numPages: null,
+    pageNumber: 1,
+  }
+
+  onDocumentLoad = ({ numPages }) => {
+    this.setState({ numPages });
+  }
+
   render() {
+    const { pageNumber, numPages } = this.state;
     return (
       <div>
         <Title
@@ -13,7 +25,19 @@ class Resume extends Component {
     <div className="row">
       <div className="col">
         <h4>Check out my resume</h4>
-        <a href="JohnSadie.pdf"><button className="btn">View Full Resume</button></a>
+        {/*<a href="JohnSadie.pdf"><button className="btn">View Full Resume</button></a>*/}
+        <div>
+          <Document
+            file={ResumePDF}
+            onLoadSuccess={this.onDocumentLoad}
+            className="resume"
+          >
+          <Page
+            pageNumber={pageNumber}
+            width="960"
+          />
+          </Document>
+        </div>
       </div>
     </div>
   </div>
