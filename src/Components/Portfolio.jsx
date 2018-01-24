86a79159ebc3549e521a61887107722b6afeb52f
johnedwardsadie <johnesadie@gmail.com>
2018-01-24 04:57:03 -0700

    feature(flexbox): adding flexbox

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -12,25 +12,25 @@ class Portfolio extends Component {
 
         <div className="flex-grid">
           <div className="col">
-            <div className="porfolioCard">
+
                 <h4>Profile</h4>
                 <p>This site was created by me to act as my virtual resume and portfolio. </p>
                 <Link className="btn" to="/Profile">Learn More</Link>
-            </div>
+
           </div>
           <div className="col">
-            <div className="porfolioCard">
+
                <h4>NAU App</h4>
                <p>Allows students to alert ITS of any conflicts/questions about technology.</p>
                <Link className="btn" to="/nauApp">Learn More</Link>
-             </div>
+
            </div>
            <div className="col">
-             <div className="porfolioCard">
+
                 <h4>Salesforce</h4>
                 <p>Redesign and recode of the internal Lightning Design System website.</p>
                 <Link className="btn" to="/salesforceInternal">Learn More</Link>
-              </div>
+
             </div>
           </div>
     </div>
