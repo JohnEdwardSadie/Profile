cc1e08c8a7265bdf07c8834f92cc3e19c24fdac4
johnedwardsadie <johnesadie@gmail.com>
2018-02-02 13:30:12 -0700

    fix(portfolio): grid system

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -11,30 +11,33 @@ class Portfolio extends Component {
           illus={portfolio}
           title="My Portfolio"
           />
+          <div className="flex-grid">
+         <div className="col">
 
-        <div className="flex-grid">
-          <div className="col">
+               <h4>Profile</h4>
+               <p>This site was created by me to act as my virtual resume and portfolio. </p>
+               <Link className="btn" to="/Website">Learn More</Link>
 
-                <h4>Profile</h4>
-                <p>This site was created by me to act as my virtual resume and portfolio. </p>
-                <Link className="btn" to="/Website">Learn More</Link>
+         </div>
+         <div className="col">
+
+              <h4>NAU App</h4>
+              <p>Alert ITS of any conflicts/questions about technology.</p>
+              <Link className="btn" to="/nauApp">Learn More</Link>
 
           </div>
           <div className="col">
 
-               <h4>NAU App</h4>
-               <p>Alert ITS of any conflicts/questions about technology.</p>
-               <Link className="btn" to="/nauApp">Learn More</Link>
+               <h4>Salesforce</h4>
+               <p>Redesign and recode of the internal Lightning Design System website.</p>
+               <Link className="btn" to="/salesforceInternal">Learn More</Link>
 
            </div>
-           <div className="col">
+         </div>
 
-                <h4>Salesforce</h4>
-                <p>Redesign and recode of the internal Lightning Design System website.</p>
-                <Link className="btn" to="/salesforceInternal">Learn More</Link>
 
-            </div>
-          </div>
+
+
     </div>
 
     )
