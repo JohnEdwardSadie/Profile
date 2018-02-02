363848870e502ea164673194d36e17a0173d6c65
johnedwardsadie <johnesadie@gmail.com>
2018-02-02 09:40:17 -0700

    fix(typography): changing typeface

--- src/Components/Title.jsx
+++ src/Components/Title.jsx
@@ -4,7 +4,7 @@ class Title extends Component {
   render() {
     return (
       <div className="illus">
-        <img className= "images" src={this.props.illus} alt="illustration"></img>
+        <img className= "images" src={this.props.illus}></img>
         <div className="title">
           <h2 className="text-justify">
             {this.props.title}
